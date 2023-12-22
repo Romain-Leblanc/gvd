7dacafe0493db47f7e4574a0d824965561522d3d
Romain-Leblanc <romleb2001@gmail.com>
2023-12-22 15:25:27 +0100

    add InterventionController + his template

---
+++ src/Controller/InterventionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\InterventionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InterventionController extends AbstractController
+{
+    #[Route('/intervention', name: 'intervention_index')]
+    public function index(InterventionRepository $interventionRepository): Response
+    {
+        $lesInterventions = $interventionRepository->findAll();
+
+        return $this->render('intervention/index.html.twig', [
+            'lesInterventions' => $lesInterventions,
+        ]);
+    }
+}
