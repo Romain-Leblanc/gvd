158704ebb4a0bfb044b68aa2cb400d9f72385991
Romain-Leblanc <romleb2001@gmail.com>
2024-01-04 19:11:45 +0100

    finish client/index route + FiltreTable

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,19 +3,36 @@
 namespace App\Controller;
 
 use App\Repository\ClientRepository;
+use App\Form\FiltreTable\FiltreTableClientType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
     #[Route('/client', name: 'client_index', methods: ['GET', 'POST'])]
-    public function index(ClientRepository $clientRepository): Response
+    public function index(ClientRepository $clientRepository, Request $request): Response
     {
         $lesClients = $clientRepository->findAll();
 
+        $form = $this->createForm(FiltreTableClientType::class, $lesClients);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupère les données du formulaire de recherche
+            $data = $request->request->get('filtre_table_client');
+            // Si au moins un filtre a été saisi, on récupère les résultats
+            if ($data['id_client'] !== ""
+                || $data['client'] !== ""
+                || $data['coordonnees'] !== ""
+                || $data['adresse_complete'] !== "") {
+                $lesClients = $clientRepository->filtreTableClient((array) $data);
+            }
+        }
+
         return $this->render('client/index.html.twig', [
-            'lesClients' => $lesClients
+            'lesClients' => $lesClients,
+            'formFiltreTable' => $form->createView()
         ]);
     }
 }
--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -21,28 +21,61 @@ class ClientRepository extends ServiceEntityRepository
         parent::__construct($registry, Client::class);
     }
 
-//    /**
-//     * @return Client[] Returns an array of Client objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /* Met à jour les informations d'un client */
+    public function updateClient(Client $client) {
+        return $this->createQueryBuilder('u')
+            ->update(Client::class, 'c')
+            ->set('c.nom', ":nom")
+            ->set('c.prenom', ":prenom")
+            ->set('c.tel', ":tel")
+            ->set('c.email', ":email")
+            ->set('c.adresse', ":adresse")
+            ->set('c.suite_adresse', ":suite_adresse")
+            ->set('c.code_postal', ":code_postal")
+            ->set('c.ville', ":ville")
+            ->set('c.num_tva', ":num_tva")
+            ->where('c.id = :id_client')
+            ->setParameter("id_client", $client->getId())
+            ->setParameter("nom", $client->getNom())
+            ->setParameter("prenom", $client->getPrenom())
+            ->setParameter("tel", $client->getTel())
+            ->setParameter("email", $client->getEmail())
+            ->setParameter("adresse", $client->getAdresse())
+            ->setParameter("suite_adresse", $client->getSuiteAdresse())
+            ->setParameter("code_postal", $client->getCodePostal())
+            ->setParameter("ville", $client->getVille())
+            ->setParameter("num_tva", $client->getNumTVA())
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 
-//    public function findOneBySomeField($value): ?Client
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /* Récupère les résultats de(s) filtre(s) saisi(s) */
+    public function filtreTableClient(array $filtre) {
+        $query = $this->createQueryBuilder('c');
+        // Ajoute les valeurs de filtres en fonction de ceux qui ont été saisis
+        if ($filtre['id_client'] !== "" || $filtre['client'] !== "") {
+            if ($filtre['id_client']) { $value = $filtre['id_client']; }
+            else { $value = $filtre['client']; }
+            $query
+                ->andWhere('c.id LIKE :id')
+                ->setParameter('id', $value)
+            ;
+        }
+        if ($filtre['coordonnees'] !== "") {
+            $value = $filtre['coordonnees'];
+            $value = str_replace(" ", "", $value);
+            $query
+                ->andWhere('c.tel LIKE :coordonnees OR c.email LIKE :coordonnees')
+                ->setParameter('coordonnees', '%'.$value.'%');
+            ;
+        }
+        if ($filtre['adresse_complete'] !== "") {
+            $value = $filtre['adresse_complete'];
+            $query
+                ->andWhere('c.adresse LIKE :adresse_complete OR c.suite_adresse LIKE :adresse_complete OR c.code_postal LIKE :adresse_complete OR c.ville LIKE :adresse_complete')
+                ->setParameter('adresse_complete', '%'.$value.'%');
+        }
+        return $query->getQuery()->getResult();
+    }
 }
